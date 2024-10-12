746af400a8cfd3ce60e156169bdce8087c77cd41
juangalante0 <juan.galante4@gmail.com>
2024-10-12 21:22:46 +0000

    Punto E resuelto

---
+++ RTA_SCRIPT_Examen_20241010/Punto_E.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+echo "MemTotal:         $(grep MemTotal /proc/meminfo | awk '{print $2 " KB"}')" > /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Galante/RTA_ARCHIVOS_Examen_20241010/Filtro_Avanzado.txt
+echo "Chassis Information:" >> /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Galante/RTA_ARCHIVOS_Examen_20241010/Filtro_Avanzado.txt
+echo "             Manufacturer: $(sudo dmidecode -s chassis-manufacturer)" >> /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Galante/RTA_ARCHIVOS_Examen_20241010/Filtro_Avanzado.txt
